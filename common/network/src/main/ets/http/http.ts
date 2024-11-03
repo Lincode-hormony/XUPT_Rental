725d2c512a30fa1404b39f6e233941330faf3655
HLQ <1826471263@qq.com>
2024-11-03 18:56:00 +0800

    详情页面实现list二级联动

--- common/network/src/main/ets/http/http.ts
+++ common/network/src/main/ets/http/http.ts
@@ -8,7 +8,7 @@ export interface AnyObject {
 }
 
 const request = axios.create({
-  baseURL: 'http://192.168.173.3:6060', // 通过 ipconfig 查询当前电脑的 ip，使用自己电脑的 ip 地址
+  baseURL: 'http://192.168.0.210:6060', // 通过 ipconfig 查询当前电脑的 ip，使用自己电脑的 ip 地址
   // baseURL: 'http://192.168.140.3:6060',
   timeout: 20000,
 })
